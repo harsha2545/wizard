99e1b913e429848661aedc8164f33e3951849f6a
harsha <harsha@gmail.com>
2019-06-24 06:55:09 +0000

    login

---
+++ src/main/java/com/wizard/login.java
@@ -0,0 +1,14 @@
+package com.wizard;
+
+/**
+ * Hello world!
+ *
+ */
+public class login
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
